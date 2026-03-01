5d18bab3c0aa5b7f4dd4218fa22564692ab11381
sevgi72 <sevgimf@code.edu.az>
2026-03-01 14:28:15 +0400

    changes

--- js/script.js
+++ js/script.js
@@ -1,35 +1,28 @@
 let draggedId = "";
-
 const boxes = document.querySelectorAll(".box");
 const zones = document.querySelectorAll(".drop-zone");
-
-
-boxes.forEach(function(box) {
-  box.addEventListener("dragstart", function() {
-    draggedId = box.id;
-  });
+boxes.forEach(function (box) {
+    box.ondragstart = function () {
+        draggedId = box.id;
+    };
 });
-
-zones.forEach(function(zone) {
-  zone.addEventListener("dragover", function(e) {
-    e.preventDefault();
-  });
-
-  zone.addEventListener("drop", function() {
-
-    let number = parseInt(draggedId.substring(3));
-    if (number >= 1 && number <= 4 && zone.id === "drop1") {
-      zone.appendChild(document.getElementById(draggedId));
-    }
-    else if (number >= 5 && number <= 8 && zone.id === "drop2") {
-      zone.appendChild(document.getElementById(draggedId));
-    }
-    else if (number >= 9 && number <= 12 && zone.id === "drop3") {
-      zone.appendChild(document.getElementById(draggedId));
-    }
-    else {
-      alert("Wrong drop!");
-    }
-
-  });
+zones.forEach(function (zone) {
+    zone.ondragover = function (e) {
+        e.preventDefault();
+    };
+    zone.ondrop = function () {
+        let number = Number(draggedId.substring(3));
+        if (number >= 1 && number <= 4 && zone.id === "d1") {
+            zone.appendChild(document.getElementById(draggedId));
+        }
+        else if (number >= 5 && number <= 8 && zone.id === "d2") {
+            zone.appendChild(document.getElementById(draggedId));
+        }
+        else if (number >= 9 && number <= 12 && zone.id === "d3") {
+            zone.appendChild(document.getElementById(draggedId));
+        }
+        else {
+            alert("Wrong drop!");
+        }
+    };
 });
